e154b9cfc7ee95def1a0704f72e8591a4d0b26f6
Germán Cruz <clz.german@gmail.com>
2016-06-25 01:07:55 -0500

    Project Upload

---
+++ Assets/Scripts/Title Screen/TitleScreenController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenController : MonoBehaviour 
+{
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Mystic
+
+Weird game. 
