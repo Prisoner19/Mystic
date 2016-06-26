c6cfbd28d413553ac3e7e5c71a698399604456ff
Germán Cruz <clz.german@gmail.com>
2016-06-26 14:16:35 -0500

    Added more structure

---
+++ Assets/Scripts/Game/Player/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Player
+{
+	public class Movement : MonoBehaviour 
+	{
+        private Model compModel;
+
+        public void Initialize()
+        { 
+
+		}
+
+        public void SetModelReference(Model compModel)
+        {
+            this.compModel = compModel;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Player/Rendering.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Player
+{
+	public class Rendering : MonoBehaviour 
+	{
+		private Model compModel;
+
+        public void Initialize()
+        { 
+
+		}
+
+        public void SetModelReference(Model compModel)
+        {
+            this.compModel = compModel;
+        }
+	}
+}
+
